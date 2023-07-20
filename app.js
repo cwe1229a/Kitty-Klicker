73467807249e147223eeec96ca0cbd4438519ac4
Courtney Webster <courtney@atlmd.net>
2023-07-20 12:39:23 -0400

    added more css and fixed pictures

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const catImage = document.getElementById("catImage");
-  const speechBubble = document.getElementById("speechBubble");
-  const API_URL = "https://api.thecatapi.com/v1/images/search";
-  const MEOW_IMAGE_URL = "https://i.imgur.com/Og3s9G0.jpg"; // New meow image URL
+// JavaScript
 
-  function fetchCatImage() {
-      fetch(API_URL)
-      .then(response => response.json())
-      .then(data => {
-          catImage.src = data[0].url;
-      })
-      .catch(error => console.error(error));
+const catContainer = document.querySelector("#catBox");
+const catImage = document.querySelector("#catImage");
+const speechBubble = document.querySelector("#speechBubble");
+
+const catApiUrl = "https://api.thecatapi.com/v1/images/search";
+const meowCatImageUrl =
+  "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg"; // replace with your preferred image URL
+
+catImage.addEventListener("click", function () {
+  catImage.classList.add("loading");
+  if (catImage.src === meowCatImageUrl) {
+    fetchCatImage();
+    speechBubble.classList.remove("show");
+  } else {
+    catImage.src = meowCatImageUrl;
+    speechBubble.classList.add("show");
+    setTimeout(fetchCatImage, 5000);
   }
-
-  function showMeowImage() {
-      catImage.src = MEOW_IMAGE_URL;
-      speechBubble.classList.add("show");
-      setTimeout(() => {
-          speechBubble.classList.remove("show");
-          fetchCatImage();
-      }, 5000);
-  }
-
-  fetchCatImage();
-
-  catImage.addEventListener("click", showMeowImage);
 });
+
+fetchCatImage();
+
+function fetchCatImage() {
+  fetch(catApiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      catImage.src = data[0].url;
+      catImage.onload = () => {
+        catImage.classList.remove("loading");
+      };
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
