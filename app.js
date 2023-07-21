26af5eae9cde0d9d043fb885f97f8b10c0471273
Courtney Webster <courtney@atlmd.net>
2023-07-20 20:24:41 -0400

    added static meow image and changed timer

--- app.js
+++ app.js
@@ -5,18 +5,20 @@ const catImage = document.querySelector("#catImage");
 const speechBubble = document.querySelector("#speechBubble");
 
 const catApiUrl = "https://api.thecatapi.com/v1/images/search";
-const meowCatImageUrl =
-  "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg"; // replace with your preferred image URL
+const meowCatImageUrl = "./bigstock-Closeup-Meowing-Ginger-Cat-On-97254212.jpg"; // replace with your image file name
 
 catImage.addEventListener("click", function () {
   catImage.classList.add("loading");
-  if (catImage.src === meowCatImageUrl) {
+  if (catImage.src.endsWith(meowCatImageUrl)) {
     fetchCatImage();
     speechBubble.classList.remove("show");
   } else {
     catImage.src = meowCatImageUrl;
     speechBubble.classList.add("show");
-    setTimeout(fetchCatImage, 5000);
+    setTimeout(() => {
+      speechBubble.classList.remove("show");
+    }, 2000);
+    setTimeout(fetchCatImage, 2000);
   }
 });
 
